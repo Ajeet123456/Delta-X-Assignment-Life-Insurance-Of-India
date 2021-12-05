88e7d4f2f08c1db5528e98440de895fc6e4b85e9
Ajeet123456 <95569390+Ajeet123456@users.noreply.github.com>
2021-12-05 17:43:24 +0530

    Add files via upload

---
+++ DELTA X.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale= 1.0">
+	<meta http-equiv="X-UA-compatible" content="IE=edge">
+	<meta name="author" content="Life Insurance of India">
+	<meta name="description" content="">
+	<meta name="keyword" content="">
+	<meta name="robot" content="INDEX,FOLLOW">
+
+
+	<title></title>
+         
+       
+	
+	<link rel="stylesheet" type="text/css" href="">
+  <style>
+         *{
+           padding: 0;
+           margin: 0';
+           
+         }
+         
+         html{
+                scroll-behavior:smooth;
+         }
+
+         body{
+          background-color: #FFD700;
+         }
+         ul{
+           display: flex;
+           flex-direction: row;
+           flex-wrap: wrap;
+         }
+         li{
+            text-decoration: none;
+            list-style: none;
+
+         }
+         ul li a{
+           margin-left: 14px;
+           display: block;
+           padding: 14px;
+           color: black;
+         }
+
+         ul li a:hover{
+           color: white;
+           border: 0px solid transparent;
+           border-radius: 14px;
+           background-color: black;
+         }
+             input{
+                     padding: 5px;
+                     
+                  
+             }
+             button{
+                     padding: 5px;
+                     border: 0px solid white;
+                     background-color: black;
+                     color: white; 
+                    cursor: pointer;
+             }
+    </style>
+
+</head>
+<body>
+  <?php
+  
+  ?>
+	 
+
+
+  <input  type="search" name="seachengine" size=70 placeholder="search here!"> <button onclick="fun()"> Search </button>
+
+  <!--<scrript>
+
+        function fun(){
+
+         $search= $_GET["search"];
+
+         alart("you search".$search);
+        }
+
+  <script> -->
+  <!DOCTYPE HTML> 
+  <html lang="en"> 
+    <head> 
+      <meta charset="UTF-8">
+       <title>Slick Web Layout</title>
+        <link rel="stylesheet" href="css/reset.css" />
+         <link rel="stylesheet" href="css/text.css" />
+          <link rel="stylesheet" href="css/960.css" />
+           <!--[if lt IE 9]> <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script> <![endif]-->
+           </head>
+            <body>
+               <header id="main-header"> 
+                 <h1 id="logo">
+                   <a href="#"></a></h1>
+                    <nav id="main-nav">
+                      <h1></h1></nav>
+                     </header> <section id="featured">
+                        <div id="descrp"></div> </section>
+                         <section id="more-products"> <h1></h1>
+                         
+                         <div class="policy"></div>
+                          <div class="policy"></div> 
+                          <div class="policy"></div>
+                         </section><!--#more-products--> 
+                         <section id="more-blurb">
+                            <div id="blurb"></div>
+                             <div id=""></div>
+                             </section>
+                              <footer id="main-footer">
+                                 <section class="footersub"></section>
+                                  <section class="footersub"></section>
+                                   <section class="footersub"></section>
+                                    <section class="footersub"></section> 
+                                    
+                                    <section id="copyright-info"></section>
+                                   </footer>
+                                   </body>
+                                    </html>
+<body>
+   <header id="main-header"> 
+     <h1 id="logo">
+       <a href="#">Life Insurance of India</a>
+      </h1> <nav id="main-nav">
+         <h1>Main page navigation</h1>
+          <ul> <li><a href="" class="current" id="top">Home</a></li>
+          <li><a href="#policy">Policy</a></li>
+            <li><a href="#about">About</a></li>
+             <li><a href="#contact">Contact</a></li> 
+            </ul> </nav> </header>
+             <section id="featured">
+                <div id="descrp">
+                   <h1>Lorem ipsum dolor sit amet, consecteteur adipiscing elit Aliquam ac duia praesent.
+
+                   </h1> <p class="button">
+                     <a href="#">View our Life Insurance</a></p>
+                      <p class="button">
+                        <a href="#">View other stuff</a></p> 
+                      </div> </section> 
+                      <section id="about">
+                         <h1>Our Life Insurance</h1>
+                          <div class="product"> 
+                            <figure>
+                               <img src="img/phone1.jpg" width="300" height="150" alt="phone number one" /> 
+                               <figcaption>
+                                  <h3>Maecenas eu felis non elit eges</h3> 
+                                  <p>Life insurance is a contract between an insurer and a policy owner.
+                                     A life insurance policy guarantees the insurer pays a sum of money
+                                      to named beneficiaries when the insured dies in exchange for the premiums paid by 
+                                      the policyholder during their lifetime.
+                                  </p> </figcaption>
+                                 </figure> 
+  </div> <div class="product">
+     <figure>
+        <img src="img/phone2.jpg" width="300" height="150" alt="phone number two" /> 
+        <figcaption> 
+          <h3>Maecenas eu felis non elit eges</h3>
+           <p>life insurance is a contract between an insurer and a policy owner.
+              A life insurance policy guarantees the insurer pays a sum of money
+              to named beneficiaries when the insured dies in exchange for the premiums paid by 
+                the policyholder during their lifetime.
+            </p> </figcaption> </figure> </div>
+             <div class="product"> 
+               <figure> 
+                 <img src="img/phone3.jpg" width="300" height="150" alt="phone number three" />
+                  <figcaption> 
+                    <h3>Maecenas eu felis non elit eges</h3>
+                     <p>life insurance is a contract between an insurer and a policy owner.
+                         A life insurance policy guarantees the insurer pays a sum of money
+                         to named beneficiaries when the insured dies in exchange for the premiums paid by 
+                         the policyholder during their lifetime.
+                          </p> </figcaption>
+                         </figure> </div>
+                         </section>
+                         <!--#more-products-->
+                          <section id="more-blurb">
+                             <div id="policy"> 
+                               <h1>Life Insurence of India. Type For.
+                               </h1>
+                                <p>Many different types of life insurance are 
+                                  available to meet all sorts of needs and preferences.
+                                   Depending on the short- or long-term needs of the person to be
+                                    insured, the major choice of whether to select temporary or permanent
+                                     life insurance is important to consider.
+  </p> <div id="blurb-widget">
+     <img src="" width="" height="" alt="" />
+      <p>Term life insurance lasts a certain number of years, then ends. 
+        You choose the term when you take out the policy.
+         Common terms are 10, 20, or 30 years. The best term life insurance policies balance
+          affordability with long-term financial strength.
+              <a href="#">Continue reading...</a></p> </article> </div>
+              <!--#blog--> </div><!--#more-blurb-->
+                     <h1>address</h1> <p>847401,Madhubani Bihar</p> 
+                    </section> <section id="contact" class="footersub"> 
+                      <h1>contact us</h1> <p>Phone: (91+) 8298665263</p> <p>ak8860770@gmail.com</p> 
+                    </section> <section class="footersub">
+                       <h1>connect with us</h1>
+                       <p class="icon"><a href="#">
+                         <img src="" width="" height="" alt="" /></a></p>
+                          <p class="icon"><a href="#"><img src="" width="" height="" alt="" /></a></p>
+                           <p class="icon"><a href="#"><img src="" width="" height="" alt="" /></a></p> 
+                           <p class="icon"><a href="#"><img src="" width="" height="" alt="" /></a></p> 
+                          </section> <section id="copyright-info">
+                             <small>©2021 Life Insurance of India.
+                                All rights reserved.
+                              </small> 
+                            </section> 
+                          </footer>
+                         </body>
+                          </html> 
+
+          <a href="#top"> Go Top </a>
+</body>
+</html>
